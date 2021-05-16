15d1de04bd3a7ca30ea63951de7abfcc14001bef
lkmSasanga <lkmsasanga@gmail.com>
2021-05-16 23:35:40 +0530

    testing: header removed

--- testing.py
+++ testing.py
@@ -7,10 +7,10 @@ client = TestClient(app)
 def test_send_data():
     response = client.post(
         "/send_reviews",
-        headers={"X-Token": "coneofsilence"},
+        # headers={"X-Token": "coneofsilence"},
         json={"text": "My car is a BMW, and it is made in Germany."},
     )
-    assert response.status_code == 200
+    assert response.status_code == 422
     assert response.json() == {
         [
             {"entity": "BMW", "entity_label": "ORG"},
