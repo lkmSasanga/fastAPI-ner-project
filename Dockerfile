aaa4c93402a2a5bb36edce623fa45c34ae8ab0b1
lkmSasanga <lkmsasanga@gmail.com>
2021-05-28 11:01:17 +0530

    docker file edited

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,21 @@
 FROM python:3.9
 
-COPY ./requirements.txt /requirements.txt
-
 WORKDIR /
 
-RUN pip3 install -r requirements.txt
+COPY requirements.txt /requirements.txt
 
-COPY . /
+RUN pip install -r requirements.txt
 
-CMD [ "python", "main.py" ]
+RUN pip install -U pip setuptools wheel
+
+#RUN pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.5/en_core_web_sm-2.2.5.tar.gz --user
+
+CMD "python -m spacy download en_core_web_sm"
+
+COPY main.py /
+
+COPY ner_of_data.py /
+
+EXPOSE 80
+
+CMD [ "uvicorn", "main:app", "--reload" ]
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,25 +1,25 @@
-import spacy
-from spacytextblob.spacytextblob import SpacyTextBlob
-
-from import_json import extract_data
-
-nlp = spacy.load("en_core_web_sm")
-
-nlp.add_pipe("spacytextblob")
-
-# extract data from json
-my_text = extract_data()
-doc = nlp(my_text)
-
-# get polarity score of the text
-sentiment_polarity = doc._.polarity
-print(sentiment_polarity)
-
-# get polarity score of the text
-sentiment_subjectivity = doc._.subjectivity
-print(sentiment_subjectivity)
-
-# get polarity score of the text
-sentiment_assessments = doc._.assessments
-print(sentiment_assessments)
-
+# import spacy
+# from spacytextblob.spacytextblob import SpacyTextBlob
+#
+# from import_json import extract_data
+#
+# nlp = spacy.load("en_core_web_sm")
+#
+# nlp.add_pipe("spacytextblob")
+#
+# # extract data from json
+# my_text = extract_data()
+# doc = nlp(my_text)
+#
+# # get polarity score of the text
+# sentiment_polarity = doc._.polarity
+# print(sentiment_polarity)
+#
+# # get polarity score of the text
+# sentiment_subjectivity = doc._.subjectivity
+# print(sentiment_subjectivity)
+#
+# # get polarity score of the text
+# sentiment_assessments = doc._.assessments
+# print(sentiment_assessments)
+#
